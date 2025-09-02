97d84fcca17c97f03e868dd5ba17cd551613ebe9
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-09-02 19:36:02 +1000

    fix format

--- live/dev/main.tf
+++ live/dev/main.tf
@@ -91,7 +91,7 @@ resource "aws_s3_bucket_policy" "site" {
 resource "aws_s3_object" "index_html" {
   bucket        = aws_s3_bucket.site.id
   key           = "index.html"
-  content       = file("${path.module}/frontend/index.html")  # <-- raw file
+  content       = data.template_file.index_html.rendered
   content_type  = "text/html"
   cache_control = "no-store, must-revalidate"
 }
