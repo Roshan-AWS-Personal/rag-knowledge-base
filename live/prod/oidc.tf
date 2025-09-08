0c9622a01cb0522907259235d512c2127bf01ec7
roshan-venkatesan-9563 <roshanvenkatesan9563@gmail.com>
2025-09-09 09:00:17 +1000

    added s3 api project to get OIDC

--- live/prod/oidc.tf
+++ live/prod/oidc.tf
@@ -47,6 +47,13 @@ data "aws_iam_policy_document" "gha_trust" {
         "repo:${var.github_owner}/${var.github_repo}:ref:refs/heads/*"
       ]
     }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = [
+        "repo:${var.github_owner}/s3-upload-api:ref:refs/heads/*"
+      ]
+    }
   }
 }
 ############################################
